dd8d07074c2ed9a9a253c4c6f987a753896b0fe5
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-04-26 19:23:29 -0700

    basic variable methods
    Displayed text, applied font color and displayed basic variable methods

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,39 @@
+window.alert("Warning: This laptop may explode if not operated by Paul");
+//pop up alert window
+
+document.write("This is a way to display text. ");
+
+var X = "Hello world! " //assigning a string value to a variable
+document.write(X);
+
+var Y = "I am a string value assigned to a variable. " //assigning a string value to a variable
+document.write(Y);
+
+document.write("\"The greatest glory in living lies not in never falling, but in rising every time we fall.\" -Nelson Mandela. ")
+// Example of escaping the character with backslash
+
+document.write("\"Your time is limited so don\'t waste it"
++ " living someone else\s life. Don\'t be"
++ " trapped by dogma - which is living"
++ " with the results of other people\'s"
++ " thinking.\" -Steve Jobs.");
+//Example of concatenating a string
+
+var A = " 35" + " years old. "
+document.write(" I am" + A);
+//Assign a concatenated string value to a variable
+
+var Family = "Fairbanks\'", Dad = "Paul", Mom = "Jen", 
+Son = "Porter", Daughter = "Isabella.";
+document.write(Daughter);
+//Assign multiple variables in one statement and displayed one
+
+var pets = " I have 2 dogs. ";
+var pets = pets.fontcolor("red");
+document.write(pets);
+//Example of assiging font color to a variable
+
+
+document.write(10+10);
+//Contains an expression
+
